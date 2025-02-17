72499205a89fc1062cf1177e3b33e6d301c8701e
Pilipus <Pilipus@gmail.com>
2025-02-17 20:50:39 +0700

    update

--- src/app/AppProvider.tsx
+++ src/app/AppProvider.tsx
@@ -26,7 +26,7 @@ export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children 
     const [inputUser, setInputUser] = useState("")
     const [isConnected, setIsConnected] = useState(false);
     const [ChatEnd, SetChatEnd] = useState(true)
-    const [version, setVersion] = useLocalStorage("version", "3d");
+    const [version, setVersion] = useLocalStorage("version", "2d");
     const [UserConncetion, SetUserConnection] = useLocalStorage("Connection", { username: "", prompt: defaultPrompt, model: "", apikey: "" })
 
     // Interaction
