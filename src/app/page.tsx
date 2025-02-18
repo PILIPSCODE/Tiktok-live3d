445b60ed91661ab05c0251e4aa0ca495bfcd152b
Pilipus <Pilipus@gmail.com>
2025-02-18 10:47:33 +0700

    test: update placement route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <main className="w-screen  overflow-x-hidden">
 
-      <div className="h-screen flex items-center font-Archivo relative ">
+      {/* <div className="h-screen flex items-center font-Archivo relative ">
         <section className={`h-screen  relative ${open ? "w-96 max-xl:w-0" : "w-screen"} flex justify-center items-center  duration-500 flex-grow`}>
           <Toast />
           <iframe src={`/${inputUser || "default"}`} className="w-full h-full rounded-lg shadow-lg" />
@@ -29,7 +29,7 @@ export default function Home() {
           <Connection />
         </section>
         <Navbar open={open} setOpen={setOpen} />
-      </div>
+      </div> */}
     </main>
   )
 }
