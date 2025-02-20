8891ba3b39c3712205b835858ac2798ff0db8cb1
Pilipus <Pilipus@gmail.com>
2025-02-20 19:37:23 +0700

    test: cek voice

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -5,13 +5,13 @@ import dynamic from "next/dynamic"
 import Toast from "@/components/Toast";
 import { useEffect, useState } from "react";
 import Gifinteraction from "@/components/Gifinteraction";
-import { useTiktokConnection } from "@/hooks/UseTiktokConnection";
 const Scene = dynamic(() => import("@/components/Mode"), { ssr: false })
 import BubleChat from "@/components/BubleChat";
 
 const RequestMusic = dynamic(() => import("@/components/RequsetMusic"), { ssr: false })
-const Connection = dynamic(() => import("@/components/ConectionStatus"), { ssr: false })
 const Navbar = dynamic(() => import("@/components/SideBar"), { ssr: false })
+
+const Connection = dynamic(() => import("@/components/ConectionStatus"), { ssr: false })
 
 
 export default function Home() {
@@ -29,11 +29,12 @@ export default function Home() {
                 <section className={`h-screen  relative ${open ? "w-96 max-xl:w-0" : "w-screen"} flex justify-center items-center  duration-500 flex-grow`}>
                     <Toast />
                     <Scene widget={true} />
-                    <div className="bottom-16 z-10 absolute max-w-80">
-                        <RequestMusic in="display" />
-                    </div>
                     <Gifinteraction />
                     <BubleChat />
+                    <div className='hidden'>
+
+                        <Navbar open={open} setOpen={setOpen} />
+                    </div>
                     <Connection />
                 </section>
             </div>
