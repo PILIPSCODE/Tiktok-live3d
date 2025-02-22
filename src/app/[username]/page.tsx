7e89717d5b2a57efb9ebd3dd208d2f834201edf6
Pilipus <Pilipus@gmail.com>
2025-02-22 10:58:59 +0700

    testing: sync voice browser and studio

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -24,7 +24,7 @@ const page = () => {
             <Mode widget={true} />
             <BubleChat />
             <div className='hidden'>
-                <Navbar open={open} setOpen={setOpen} />
+                {/* <Navbar open={open} setOpen={setOpen} /> */}
             </div>
         </div>
     )
