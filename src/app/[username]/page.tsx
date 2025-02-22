c9e8dcfdd674ad3236292b51dcac020b165840c9
Pilipus <Pilipus@gmail.com>
2025-02-22 10:37:39 +0700

    test rate studio and browser

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,66 +1,36 @@
-// "use client"
-// import BubleChat from '@/components/BubleChat'
-// import Mode from '@/components/Mode'
-// const Navbar = dynamic(() => import("@/components/SideBar"), { ssr: false })
-// import { socket } from '@/utils/socket'
-// import dynamic from 'next/dynamic'
-// import { useParams } from 'next/navigation'
-// import React, { useEffect, useState } from 'react'
-
-// const page = () => {
-//     const params = useParams()
-//     const [open, setOpen] = useState(false)
-
-//     useEffect(() => {
-//         socket.emit("EmbedJoin", params.username)
-//     }, [])
-
-//     return (
-//         <div className="flex justify-center font-Archivo">
-//             <Mode widget={true} />
-//             <BubleChat />
-//             <div className='hidden'>
-//                 <Navbar open={open} setOpen={setOpen} />
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default page
-
-
-
 "use client"
+import BubleChat from '@/components/BubleChat'
+import Mode from '@/components/Mode'
+import { useCharacter } from '@/hooks/useCharacter'
+const Navbar = dynamic(() => import("@/components/SideBar"), { ssr: false })
+import { socket } from '@/utils/socket'
+import dynamic from 'next/dynamic'
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
-import { useEffect, useState } from "react";
-
-export default function VoiceDebugger() {
-    const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
+const page = () => {
+    const params = useParams()
+    const [open, setOpen] = useState(false)
+    const { setVoiceSettings } = useCharacter()
 
 
     useEffect(() => {
-        setTimeout(() => {
-            const loadVoices = () => setVoices(speechSynthesis.getVoices());
-            loadVoices();
-        }, 3000); // Delay untuk memastikan voice list sudah dimuat
-        if (typeof window !== "undefined" && speechSynthesis) {
-            const loadVoices = () => setVoices(speechSynthesis.getVoices());
-            loadVoices();
-
-            speechSynthesis.onvoiceschanged = loadVoices;
-        }
-    }, []);
+        socket.emit("EmbedJoin", params.username)
+        setVoiceSettings({ voice: "", rate: "1.6", pitch: "1", volume: "1" })
+    }, [])
 
     return (
-        <div className="absolute top-5 left-5 bg-black text-white p-4 rounded-lg z-50">
-            <h2 className="text-lg font-bold">Available Voices:</h2>
-            <ul>
-                {voices.length > 0 ? voices.map((voicess, index) => (
-                    <li key={index}>{voicess.voiceURI}</li>
-                )) : <li>Loading voices...</li>}
-            </ul>
+        <div className="flex justify-center font-Archivo">
+            <Mode widget={true} />
+            <BubleChat />
+            <div className='hidden'>
+                <Navbar open={open} setOpen={setOpen} />
+            </div>
         </div>
-    );
-
-
+    )
 }
+
+export default page
+
+
+
